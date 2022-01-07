8786d333be2b8e20dab06d1bf7ee7f8f051dc095
Stephen <sdarcy29@hotmail.com>
2022-01-07 16:48:13 +0000

    commit

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -60,6 +60,12 @@ describe("new game works correctly", () => {
     test("should display zero with element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        };
+    });
 });
 
 describe("game play works correctly", () => {
